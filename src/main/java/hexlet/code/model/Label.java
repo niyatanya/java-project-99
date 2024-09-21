f1b723c4d9e2c8597a6bbf7c48c81bf235b84b3c
Naumenko Tatiana <jouk@list.ru>
2024-09-21 15:58:43 +0300

    Correct style issues

--- src/main/java/hexlet/code/model/Label.java
+++ src/main/java/hexlet/code/model/Label.java
@@ -5,7 +5,6 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Column;
 
 import jakarta.validation.constraints.Size;
@@ -17,8 +16,6 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -41,9 +38,6 @@ public class Label implements BaseEntity {
 
     @CreatedDate
     private LocalDateTime createdAt;
-
-//    @ManyToMany(mappedBy = "labels")
-//    private Set<Task> tasks = new HashSet<>();
 
     public Label(String name) {
         this.name = name;
